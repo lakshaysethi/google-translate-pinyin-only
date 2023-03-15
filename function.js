0bf051b8f90fb9828cca002c0db87c683740cf32
lakshaysethi <45325278+lakshaysethi@users.noreply.github.com>
2023-03-15 19:56:36 +1300

    Create Art

---
+++ function.js
@@ -0,0 +1,6 @@
+const class_name_from_google = ""
+const our_wonderful_function= () => {
+  let the_element_with_all_the_crazy_kungfu = document.getElementsByClassName(class_name_from_google)[0]
+  the_element_with_all_the_crazy_kungfu.style.display = "none"
+}
+setInterval(  our_wonderful_function, 500)
