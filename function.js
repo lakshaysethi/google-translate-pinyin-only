19eb7250d7b94c637be0e90b6e4ef14e5d62501d
lakshaysethi <45325278+lakshaysethi@users.noreply.github.com>
2023-04-02 18:59:22 +1200

    Update function.js

--- function.js
+++ function.js
@@ -1,4 +1,4 @@
-const class_name_from_google = ""
+const class_name_from_google = "lRu31"
 const our_wonderful_function= () => {
   let the_element_with_all_the_crazy_kungfu = document.getElementsByClassName(class_name_from_google)[0]
   the_element_with_all_the_crazy_kungfu.style.display = "none"
